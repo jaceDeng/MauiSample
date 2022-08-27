86a0308ded1c29e0f202244c79f84d62a3f7c9bf
JSON <micro.man@live.cn>
2022-08-27 22:16:45 +0800

    演示签入文件资源并显示

---
+++ EmbedRes/MauiApp3/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿
+
+
+using Microsoft.Maui.Controls;
+
+namespace MauiApp3;
+
+public partial class MainPage : ContentPage
+{
+    int count = 0;
+
+    public MainPage()
+    {
+        InitializeComponent();
+
+
+    }
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        Load();
+    }
+    protected async Task Load()
+    {
+        base.OnAppearing();
+
+
+        name.Text = await ReadRes("name.xml");
+
+        var html = await ReadRes("index.html");
+        web.Source = new HtmlWebViewSource() { Html = html };
+    }
+
+    private async Task<string> ReadRes(string key)
+    {
+        using var stream = await FileSystem.OpenAppPackageFileAsync(key);
+        using var reader = new StreamReader(stream);
+
+        var contents = reader.ReadToEnd();
+        return contents;
+    }
+
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+        count++;
+
+        if (count == 1)
+            CounterBtn.Text = $"Clicked {count} time";
+        else
+            CounterBtn.Text = $"Clicked {count} times";
+
+        SemanticScreenReader.Announce(CounterBtn.Text);
+    }
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # MauiSample
 主要对maui 做一些demo 和实例代码
+
+
+## EmbedRes 
+主要是测试签入raw 文件资源
+演示 加载html到webview中
